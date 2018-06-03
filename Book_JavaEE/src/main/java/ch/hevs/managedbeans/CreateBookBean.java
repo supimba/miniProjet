5501c0e023b17af61925230c8e06d2cabfbff70c
Supimba <batterteam@gmail.com>
2018-06-03 17:51:51 +0200

    Create book with category and writer

--- Book_JavaEE/src/main/java/ch/hevs/managedbeans/CreateBookBean.java
+++ Book_JavaEE/src/main/java/ch/hevs/managedbeans/CreateBookBean.java
@@ -22,48 +22,57 @@ public class CreateBookBean {
 
 	@EJB
 	private BookShelf bookShelf;
-	private String test; 
+	private String test;
 	private Set<Category> categories;
-	private Category categroy; 
+	private Category categroy;
 	private Set<Writer> writers;
 	private List<String> writerSelectedId;
 
 
-	private Book book = new Book(); 
-	
+	private List<String> categorySelectedId; 
+
+	private Book book = new Book();
+
 	@PostConstruct
 	public void initialize() throws NamingException {
 		// use JNDI to inject reference to bank EJB
 		InitialContext ctx = new InitialContext();
-		bookShelf = (BookShelf) ctx.lookup("java:global/Book_JavaEE-0.0.1-SNAPSHOT/BookShelfBean!ch.hevs.bookshelf.BookShelf");
-		 
+		bookShelf = (BookShelf) ctx
+				.lookup("java:global/Book_JavaEE-0.0.1-SNAPSHOT/BookShelfBean!ch.hevs.bookshelf.BookShelf");
+
 		this.writers = bookShelf.getWriters();
-		for(Writer w : writers)
-			System.out.println("Writers: " + w.getFirstname());
 		this.categories = bookShelf.getCategories();
 	}
 
 	public List<String> getWriterSelectedId() {
 		return writerSelectedId;
 	}
-	
+
 	public void setWriterSelectedId(List<String> writerSelectedId) {
 		this.writerSelectedId = writerSelectedId;
 	}
+	
+	public List<String> getCategorySelectedId() {
+		return categorySelectedId;
+	}
 
+	public void setCategorySelectedId(List<String> categorySelectedId) {
+		this.categorySelectedId = categorySelectedId;
+	}
 
-	public void insertBook(Book book){
-//		Writer writer = bookShelf.getWriter(Long.valueOf(writerSelectedId)); 
-		for(String writerId : writerSelectedId)
+	public void insertBook(Book book) {
+		for (String writerId : writerSelectedId)
 			book.addWriter(bookShelf.getWriter(Long.valueOf(writerId)));
 		
-		bookShelf.insertBook(book);		
+		for(String categoryId : categorySelectedId)
+			book.addCategory(bookShelf.getCategory(Long.valueOf(categoryId)));
+
+		bookShelf.insertBook(book);
 	}
 
 	public Set<Writer> getWriters() {
 		return bookShelf.getWriters();
 	}
-	
 
 	public Set<Category> getCategories() {
 		return bookShelf.getCategories();
